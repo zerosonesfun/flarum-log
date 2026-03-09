7dbc6221e18767de7ba67b423fe0045af1a3be88
Billy Wilcosky <41351317+zerosonesfun@users.noreply.github.com>
2026-03-09 17:11:29 -0400

    Your commit message

--- 2.0/js/src/forum/extend.js
+++ 2.0/js/src/forum/extend.js
@@ -4,4 +4,4 @@
 import Routes from 'ext:flarum/common/extend/Routes';
 import DrinkLogsUserPage from './components/DrinkLogsUserPage';
 
-export default [new Routes().add('zerosonesfun.drink-logs', '/u/:username/drink-logs', DrinkLogsUserPage)];
+export default [new Routes().add('zerosonesfun.drink-logs', '/u/:username/drink-logs', <DrinkLogsUserPage />)];
--- js/src/forum/extend.js
+++ js/src/forum/extend.js
@@ -1,5 +1,4 @@
-import Extend from 'flarum/common/extend';
 import Routes from 'flarum/common/extend/Routes';
 import DrinkLogsUserPage from './components/DrinkLogsUserPage';
 
-export default [new Routes().add('zerosonesfun.drink-logs', '/u/:username/drink-logs', DrinkLogsUserPage)];
+export default [new Routes().add('zerosonesfun.drink-logs', '/u/:username/drink-logs', <DrinkLogsUserPage />)];
